8aad4df2c5bbb2207b155443a3c3790ca7112fb5
Garfield Carneiro <carneiro@yahoo-inc.com>
2013-09-04 00:46:54 +0530

    Regex not working ;  but gotto sleep

--- tmuxer.pl
+++ tmuxer.pl
@@ -5,7 +5,12 @@ use warnings ;
 
 my $TMUX='/usr/bin/tmux';
 
+$ARGV[0] =~ /.*\./;
+my $session_name = $1;
+
+
 print "Got Argument: $ARGV[0]\n";
+print "Got Regex: $session_name\n";
 
 open(HF, "$ARGV[0]") ; #Host File
 
