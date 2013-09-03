e2a5078ea6a8e47e37ca913671187e4c657897c8
Garfield Carneiro <carneiro@yahoo-inc.com>
2013-09-04 00:25:13 +0530

    Reads from file

--- tmuxer.pl
+++ tmuxer.pl
@@ -3,9 +3,15 @@
 use strict ;
 use warnings ;
 
-$TMUX='/usr/bin/tmux';
+my $TMUX='/usr/bin/tmux';
 
-print "Got Argument: $ARGV[0]\n" ;
+print "Got Argument: $ARGV[0]\n";
 
 open(HF, "$ARGV[0]") ; #Host File
 
+while(<HF>) {
+    chomp;
+    print "$_\n";
+}
+
+close(HF); 
