7e9c9d0c9a6b98cad6e23d3b185b4ac945555e04
Gary Carneiro <gary.carneiro@gmail.com>
2013-09-23 23:46:59 +0530

    Lets start over !!

--- genTY.rb
+++ genTY.rb
@@ -1,1 +1,12 @@
 #!/usr/bin/env ruby
+
+
+if ARGV[0].nil?
+    print "Pass some hostfile Matey !!"
+    exit 1
+else
+    print "Awright got file #{ARGV[0]}\n"
+end
+
+
+File.readlines(ARGV[0]).each { |host| print host }
