527d15f968a31136e8dfdf7af1e8f7089aa79614
GaryCarneiro <gary.carneiro@gmail.com>
2013-10-12 00:36:47 +0530

    Working but sloppy code

--- genTY.rb
+++ genTY.rb
@@ -8,18 +8,26 @@ $cli={}
 
 OptionParser.new do | opts |
   opts.banner = "Usage #{$0} [ -c --cmdfile <FILEPATH> ] | [--hostsfile <FILEPATH> ]"
-  opts.on("-c", "--cmdfile=val", String) { |cmdfile|    if not cmdfile.nil? then  $cli['cmdfile'] = cmdfile end  }
-  opts.on("-f", "--hostfile=val", String) { |hostfile | if not hostfile.nil? then  $cli['hostfile'] = hostfile end}
+  opts.on("-f", "--hostfile=val", String) { |hostfile| $cli['hostfile'] = hostfile }
+  opts.on("-c", "--cmdfile=val", String)  { |cmdfile| $cli['cmdfile'] = cmdfile }
   opts.on_tail("-h", "--help")
   opts.parse!
 end
 
 session =  {}
-#print hostfile
 
-session['name'] =  $cli['hostfile']
-session['cmd'] =  $cli['cmdfile']
-#http://www.yaml.org/YAML_for_ruby.html
-# => session['root'] = Dir.pwd
-print session
+#print $cli
+
+if $cli.key? "hostfile"
+  session['name'] = $cli['hostfile'].split('.txt')
+elsif $cli.key? "cmdfile"
+  session['name'] = $cli['cmdfile'].split('.txt')
+else
+    print "Please pass either commandfile or hostfile"
+    exit
+end
+
+session['root'] = Dir.pwd
+
+# print session
 
