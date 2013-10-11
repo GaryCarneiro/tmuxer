a9a1026d94ee28ab6a58c9f6edfc96483cb53671
GaryCarneiro <gary.carneiro@gmail.com>
2013-10-12 00:10:01 +0530

    [NW]

--- genTY.rb
+++ genTY.rb
@@ -1,17 +1,25 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require 'yaml'
 
-
-$hostFile = ''
-$cmdFile = ''
+$cli={}
 
 
 OptionParser.new do | opts |
-  opts.banner = "Usage #{$ARGC} [ -c --cmdfile <FILEPATH> ] | [--hostsfile <FILEPATH> ]"
-  opts.on("-c", "--cmdfile=val", String) { | $cmdFile | }
-  opts.on("-f", "--hostfile=val", String) { |$hostFile| }
+  opts.banner = "Usage #{$0} [ -c --cmdfile <FILEPATH> ] | [--hostsfile <FILEPATH> ]"
+  opts.on("-c", "--cmdfile=val", String) { |cmdfile|    if not cmdfile.nil? then  $cli['cmdfile'] = cmdfile end  }
+  opts.on("-f", "--hostfile=val", String) { |hostfile | if not hostfile.nil? then  $cli['hostfile'] = hostfile end}
   opts.on_tail("-h", "--help")
   opts.parse!
 end
+
+session =  {}
+#print hostfile
+
+session['name'] =  $cli['hostfile']
+session['cmd'] =  $cli['cmdfile']
+#http://www.yaml.org/YAML_for_ruby.html
+# => session['root'] = Dir.pwd
+print session
 
